b976d64890086999ec46de123726a5109349533d
haopc1404@gmail.com <haopc1404@gmail.com>
2025-03-25 11:07:52 +0700

    update

---
+++ 52_Hao_N2_B3_Selenium_WebDriver/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace _52_Hao_N2_B3_Selenium_WebDriver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Open_52_Hao = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txt_url_52_Hao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_open_url_52_hao = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.txt_open_url_52_Hao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_notext_url_52_Hao = new System.Windows.Forms.Button();
+            this.btn_Close_52_hao = new System.Windows.Forms.Button();
+            this.btn_CloseMHDen_52_Hao = new System.Windows.Forms.Button();
+            this.btn_CloseAllWindow_52_Hao = new System.Windows.Forms.Button();
+            this.txt_hien_thi_52_Hao = new System.Windows.Forms.TextBox();
+            this.btn_lay_title_52_Hao = new System.Windows.Forms.Button();
+            this.btn_lay_pagesource_52_Hao = new System.Windows.Forms.Button();
+            this.btn_Back_Forward_52_Hao = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Open_52_Hao
+            // 
+            this.btn_Open_52_Hao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Open_52_Hao.Location = new System.Drawing.Point(263, 131);
+            this.btn_Open_52_Hao.Name = "btn_Open_52_Hao";
+            this.btn_Open_52_Hao.Size = new System.Drawing.Size(207, 68);
+            this.btn_Open_52_Hao.TabIndex = 1;
+            this.btn_Open_52_Hao.Text = "Open Browser";
+            this.btn_Open_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_Open_52_Hao.Click += new System.EventHandler(this.btn_Open_52_Hao_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(802, 532);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btn_Open_52_Hao);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(794, 506);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Bai 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txt_url_52_Hao);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.btn_open_url_52_hao);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(794, 506);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Bai 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // txt_url_52_Hao
+            // 
+            this.txt_url_52_Hao.Location = new System.Drawing.Point(276, 81);
+            this.txt_url_52_Hao.Name = "txt_url_52_Hao";
+            this.txt_url_52_Hao.Size = new System.Drawing.Size(180, 20);
+            this.txt_url_52_Hao.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "url";
+            // 
+            // btn_open_url_52_hao
+            // 
+            this.btn_open_url_52_hao.Location = new System.Drawing.Point(300, 132);
+            this.btn_open_url_52_hao.Name = "btn_open_url_52_hao";
+            this.btn_open_url_52_hao.Size = new System.Drawing.Size(100, 26);
+            this.btn_open_url_52_hao.TabIndex = 3;
+            this.btn_open_url_52_hao.Text = "open url";
+            this.btn_open_url_52_hao.UseVisualStyleBackColor = true;
+            this.btn_open_url_52_hao.Click += new System.EventHandler(this.btn_open_url_52_hao_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.btn_Back_Forward_52_Hao);
+            this.tabPage3.Controls.Add(this.btn_lay_pagesource_52_Hao);
+            this.tabPage3.Controls.Add(this.btn_lay_title_52_Hao);
+            this.tabPage3.Controls.Add(this.txt_hien_thi_52_Hao);
+            this.tabPage3.Controls.Add(this.btn_CloseAllWindow_52_Hao);
+            this.tabPage3.Controls.Add(this.btn_CloseMHDen_52_Hao);
+            this.tabPage3.Controls.Add(this.btn_Close_52_hao);
+            this.tabPage3.Controls.Add(this.btn_notext_url_52_Hao);
+            this.tabPage3.Controls.Add(this.txt_open_url_52_Hao);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(794, 506);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Bai 3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
+            // 
+            // txt_open_url_52_Hao
+            // 
+            this.txt_open_url_52_Hao.Location = new System.Drawing.Point(280, 99);
+            this.txt_open_url_52_Hao.Name = "txt_open_url_52_Hao";
+            this.txt_open_url_52_Hao.Size = new System.Drawing.Size(173, 20);
+            this.txt_open_url_52_Hao.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "url";
+            // 
+            // btn_notext_url_52_Hao
+            // 
+            this.btn_notext_url_52_Hao.Location = new System.Drawing.Point(313, 160);
+            this.btn_notext_url_52_Hao.Name = "btn_notext_url_52_Hao";
+            this.btn_notext_url_52_Hao.Size = new System.Drawing.Size(75, 23);
+            this.btn_notext_url_52_Hao.TabIndex = 5;
+            this.btn_notext_url_52_Hao.Text = "open url";
+            this.btn_notext_url_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_notext_url_52_Hao.Click += new System.EventHandler(this.btn_notext_url_52_Hao_Click);
+            // 
+            // btn_Close_52_hao
+            // 
+            this.btn_Close_52_hao.Location = new System.Drawing.Point(88, 233);
+            this.btn_Close_52_hao.Name = "btn_Close_52_hao";
+            this.btn_Close_52_hao.Size = new System.Drawing.Size(140, 23);
+            this.btn_Close_52_hao.TabIndex = 6;
+            this.btn_Close_52_hao.Text = "Close trình duyệt";
+            this.btn_Close_52_hao.UseVisualStyleBackColor = true;
+            this.btn_Close_52_hao.Click += new System.EventHandler(this.btn_Close_52_hao_Click);
+            // 
+            // btn_CloseMHDen_52_Hao
+            // 
+            this.btn_CloseMHDen_52_Hao.Location = new System.Drawing.Point(280, 233);
+            this.btn_CloseMHDen_52_Hao.Name = "btn_CloseMHDen_52_Hao";
+            this.btn_CloseMHDen_52_Hao.Size = new System.Drawing.Size(176, 23);
+            this.btn_CloseMHDen_52_Hao.TabIndex = 7;
+            this.btn_CloseMHDen_52_Hao.Text = "Tắt màn hình đen và lấy url";
+            this.btn_CloseMHDen_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_CloseMHDen_52_Hao.Click += new System.EventHandler(this.btn_CloseMHDen_52_Hao_Click);
+            // 
+            // btn_CloseAllWindow_52_Hao
+            // 
+            this.btn_CloseAllWindow_52_Hao.Location = new System.Drawing.Point(521, 233);
+            this.btn_CloseAllWindow_52_Hao.Name = "btn_CloseAllWindow_52_Hao";
+            this.btn_CloseAllWindow_52_Hao.Size = new System.Drawing.Size(143, 23);
+            this.btn_CloseAllWindow_52_Hao.TabIndex = 8;
+            this.btn_CloseAllWindow_52_Hao.Text = "Đóng mọi cửa sổ liên kết";
+            this.btn_CloseAllWindow_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_CloseAllWindow_52_Hao.Click += new System.EventHandler(this.btn_CloseAllWindow_52_Hao_Click);
+            // 
+            // txt_hien_thi_52_Hao
+            // 
+            this.txt_hien_thi_52_Hao.Location = new System.Drawing.Point(88, 333);
+            this.txt_hien_thi_52_Hao.Name = "txt_hien_thi_52_Hao";
+            this.txt_hien_thi_52_Hao.Size = new System.Drawing.Size(271, 20);
+            this.txt_hien_thi_52_Hao.TabIndex = 9;
+            this.txt_hien_thi_52_Hao.TextChanged += new System.EventHandler(this.txt_hien_thi_52_Hao_TextChanged);
+            // 
+            // btn_lay_title_52_Hao
+            // 
+            this.btn_lay_title_52_Hao.Location = new System.Drawing.Point(404, 330);
+            this.btn_lay_title_52_Hao.Name = "btn_lay_title_52_Hao";
+            this.btn_lay_title_52_Hao.Size = new System.Drawing.Size(120, 23);
+            this.btn_lay_title_52_Hao.TabIndex = 10;
+            this.btn_lay_title_52_Hao.Text = "lấy title";
+            this.btn_lay_title_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_lay_title_52_Hao.Click += new System.EventHandler(this.btn_lay_title_52_Hao_Click);
+            // 
+            // btn_lay_pagesource_52_Hao
+            // 
+            this.btn_lay_pagesource_52_Hao.Location = new System.Drawing.Point(149, 408);
+            this.btn_lay_pagesource_52_Hao.Name = "btn_lay_pagesource_52_Hao";
+            this.btn_lay_pagesource_52_Hao.Size = new System.Drawing.Size(184, 23);
+            this.btn_lay_pagesource_52_Hao.TabIndex = 11;
+            this.btn_lay_pagesource_52_Hao.Text = "Lấy pagesource";
+            this.btn_lay_pagesource_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_lay_pagesource_52_Hao.Click += new System.EventHandler(this.btn_lay_pagesource_52_Hao_Click);
+            // 
+            // btn_Back_Forward_52_Hao
+            // 
+            this.btn_Back_Forward_52_Hao.Location = new System.Drawing.Point(434, 408);
+            this.btn_Back_Forward_52_Hao.Name = "btn_Back_Forward_52_Hao";
+            this.btn_Back_Forward_52_Hao.Size = new System.Drawing.Size(136, 23);
+            this.btn_Back_Forward_52_Hao.TabIndex = 12;
+            this.btn_Back_Forward_52_Hao.Text = "Back Forward";
+            this.btn_Back_Forward_52_Hao.UseVisualStyleBackColor = true;
+            this.btn_Back_Forward_52_Hao.Click += new System.EventHandler(this.btn_Back_Forward_52_Hao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Open_52_Hao;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txt_url_52_Hao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_open_url_52_hao;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox txt_open_url_52_Hao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_notext_url_52_Hao;
+        private System.Windows.Forms.Button btn_Close_52_hao;
+        private System.Windows.Forms.Button btn_CloseMHDen_52_Hao;
+        private System.Windows.Forms.Button btn_CloseAllWindow_52_Hao;
+        private System.Windows.Forms.Button btn_lay_title_52_Hao;
+        private System.Windows.Forms.TextBox txt_hien_thi_52_Hao;
+        private System.Windows.Forms.Button btn_lay_pagesource_52_Hao;
+        private System.Windows.Forms.Button btn_Back_Forward_52_Hao;
+    }
+}
+
---
+++ 52_Hao_N2_B3_Selenium_WebDriver/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace _52_Hao_N2_B3_Selenium_WebDriver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Open_52_Hao_Click(object sender, EventArgs e)
+        {
+            // dieu huong trinh duyet
+            //bai 1: 52_Hao dieu huong den trang google
+            IWebDriver driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Navigate().GoToUrl("https://www.google.com/");        
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_open_url_52_hao_Click(object sender, EventArgs e)
+        {
+            //Bai 2: Dieu huong den 1 trang web VnEpress bang cach go vao textbox
+            IWebDriver drive_52_Hao = new ChromeDriver();
+            drive_52_Hao.Url = txt_url_52_Hao.Text;
+            drive_52_Hao.Navigate();
+        }
+
+        // Khai báo biến toàn cục cho IWebDriver
+        private IWebDriver driver_52_Hao;
+
+        private void btn_notext_url_52_Hao_Click(object sender, EventArgs e)
+        {
+            // Bài 3 a: Không nhập url, nhấn open url, textbox sẽ nhận url mà bạn code
+            driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Navigate().GoToUrl("http://vnexpress.net");
+        }
+
+        private void btn_Close_52_hao_Click(object sender, EventArgs e)
+        {
+            // Bài 3 b: Close trình duyệt 
+            driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Close();
+        }
+
+        private void btn_CloseMHDen_52_Hao_Click(object sender, EventArgs e)
+        {
+            //Bai 3 c: tat man hinh den
+            ChromeDriverService chrome_52_Hao = ChromeDriverService.CreateDefaultService();
+            chrome_52_Hao.HideCommandPromptWindow = true;
+            //dieu huong trinh duyet
+            IWebDriver driver_52_Hao = new ChromeDriver(chrome_52_Hao);
+            driver_52_Hao.Navigate().GoToUrl("http://vnexpress.net");
+
+            //Bai 3 e: lay url
+            String link_hien_tai_52_Hao = driver_52_Hao.Url;
+            Console.WriteLine(link_hien_tai_52_Hao);
+        }
+
+        private void btn_CloseAllWindow_52_Hao_Click(object sender, EventArgs e)
+        {
+            //Bai 3 d: Tat moi cua so lien ket
+            driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Quit();
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_lay_title_52_Hao_Click(object sender, EventArgs e)
+        {
+            //Bai 3 f: lay title
+            //Dieu huong den 1 trinh duyet
+            driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Navigate().GoToUrl("http://vnexpress.net");
+            //lay title
+            String urlTile_52_hao = driver_52_Hao.Title;
+            txt_hien_thi_52_Hao.Text = urlTile_52_hao;
+        }
+
+        private void txt_hien_thi_52_Hao_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_lay_pagesource_52_Hao_Click(object sender, EventArgs e)
+        {
+            //Bai 3 g: lay pagesource
+            //Dieu huong den 1 trinh duyet
+            driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Navigate().GoToUrl("http://vnexpress.net");
+            //Lay page source
+            String PageSource_52_hao = driver_52_Hao.PageSource;
+            Console.WriteLine(PageSource_52_hao);
+        }
+
+        private void btn_Back_Forward_52_Hao_Click(object sender, EventArgs e)
+        {
+            //Bai 3 h: back forward
+            //Dieu huong den 1 trinh duyet
+            driver_52_Hao = new ChromeDriver();
+            driver_52_Hao.Navigate().GoToUrl("http://vnexpress.net");
+            Thread.Sleep(1000);
+            driver_52_Hao.Navigate().GoToUrl("http://ou.edu.vn/");
+            Thread.Sleep(5000);
+            driver_52_Hao.Navigate().Back();
+            Thread.Sleep(1000);
+            driver_52_Hao.Navigate().Forward();
+            Thread.Sleep(1000);
+            driver_52_Hao.Navigate().Refresh();
+        }
+    }
+}
